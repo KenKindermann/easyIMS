ab97a1c2190a7ee1349453baeba2905f45b980b2
kenkindermann <ken.kindermann@gmail.com>
2024-02-02 21:49:10 +0100

    add route /:key & function to get customer by specific value

--- backend/controllers/customers.js
+++ backend/controllers/customers.js
@@ -8,3 +8,20 @@ export const getAllCustomers = async (req, res) => {
     res.status(500).json({ message: error });
   }
 };
+
+export const getCustomerByValue = async (req, res) => {
+  const { key } = req.params;
+  const { value } = req.query;
+
+  try {
+    const response = await pool.query(`SELECT * FROM customers WHERE ${key} = $1`, [value]);
+
+    if (response.rows.length > 0) {
+      res.json(response.rows);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
--- backend/routes/customersRouter.js
+++ backend/routes/customersRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllCustomers } from "../controllers/customers.js";
+import { getAllCustomers, getCustomerByValue } from "../controllers/customers.js";
 
 const customersRouter = Router();
 
 customersRouter.route("/").get(getAllCustomers);
+customersRouter.route("/:key").get(getCustomerByValue);
 
 export default customersRouter;
