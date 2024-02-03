274359773cded5e56856589911da0409634dd8bd
kenkindermann <ken.kindermann@gmail.com>
2024-02-03 14:01:18 +0100

    add post route to products router

--- backend/routes/productsRouter.js
+++ backend/routes/productsRouter.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { getAll } from "../controllers/general.js";
+import { addNewProduct } from "../controllers/products.js";
 
 const customersRouter = Router();
 
-customersRouter.route("/").get((req, res) => getAll("products", req, res));
+customersRouter
+  .route("/")
+  .get((req, res) => getAll("products", req, res))
+  .post(addNewProduct);
 
 export default customersRouter;
