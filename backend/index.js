5bcf46c2fc1002db35b22111a9f8569cc42a10ed
kenkindermann <ken.kindermann@gmail.com>
2024-02-03 13:48:46 +0100

    add products router to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import customerRouter from "./routes/customersRouter.js";
+import productsRouter from "./routes/productsRouter.js";
 
 const app = express();
 const PORT = 8000;
@@ -8,5 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/customers", customerRouter);
+app.use("/products", productsRouter);
 
 app.listen(PORT, console.log(`Server is running on Port: ${PORT}`));
