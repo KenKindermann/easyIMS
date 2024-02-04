adfce5d599dafdd808c3f9edc3e3e88e0f4d5fef
kenkindermann <ken.kindermann@gmail.com>
2024-02-04 20:51:43 +0100

    change routes in app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,15 +4,33 @@ import Navbar from "./components/navigation/Navbar";
 import DarkOverlay from "./components/global/DarkOverlay";
 import ControlPanel from "./components/controlPanel/ControlPanel";
 import Table from "./components/table/Table";
+import { useState } from "react";
 
 function App() {
   return (
     <>
       <Navbar />
-      <ControlPanel />
-      <Table />
+
       <Routes>
-        <Route path="/customers" />
+        <Route
+          path="/customers"
+          element={
+            <>
+              <ControlPanel />
+              <Table table={"customers"} />
+            </>
+          }
+        />
+
+        <Route
+          path="/products"
+          element={
+            <>
+              <ControlPanel />
+              <Table table={"products"} />
+            </>
+          }
+        />
       </Routes>
       <DarkOverlay />
     </>
