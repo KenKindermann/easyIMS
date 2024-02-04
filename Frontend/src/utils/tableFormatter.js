5b09371b693b0eeb84fc9dd06a3c3222bc437f1d
kenkindermann <ken.kindermann@gmail.com>
2024-02-04 22:29:16 +0100

    format table values by tableFormatter

---
+++ Frontend/src/utils/tableFormatter.js
@@ -0,0 +1,24 @@
+export const customers = {
+  labels: [
+    "Customer No",
+    "Fist name",
+    "Last name",
+    "Street",
+    "Zip code",
+    "City",
+    "Shipping street",
+    "Shipping zip code",
+    "Shipping city",
+  ],
+  keys: [
+    "id",
+    "firstname",
+    "lastname",
+    "street",
+    "zipcode",
+    "city",
+    "shippingstreet",
+    "shippingzipcode",
+    "shippingcity",
+  ],
+};
