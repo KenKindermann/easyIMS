37ddf24e80c14ab9e61337c1ca287e3dda0599e0
kenkindermann <ken.kindermann@gmail.com>
2024-02-04 14:07:42 +0100

    toggle bar in nav

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -3,8 +3,19 @@ import "../../style/navbar.css";
 const Navbar = () => {
   return (
     <nav>
-      <p>easyIWS</p>
-      <p>Ken Kindermann</p>
+      <div className="navbar">
+        <div className="toggle-bar-and-page">
+          <div className="toggle-bar">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <p>Customers</p>
+        </div>
+        <div>
+          <p>Ken Kindermann</p>
+        </div>
+      </div>
     </nav>
   );
 };
