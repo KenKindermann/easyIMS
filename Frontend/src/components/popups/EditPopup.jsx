3170747985e17cd082e618fee7d45de50103a9a2
kenkindermann <ken.kindermann@gmail.com>
2024-02-05 20:59:25 +0100

    input values are filled by checked item

--- Frontend/src/components/popups/EditPopup.jsx
+++ Frontend/src/components/popups/EditPopup.jsx
@@ -2,22 +2,24 @@ import { useContext, useEffect } from "react";
 import "../../style/popup.css";
 import { TableContext } from "../../provider/TableContext";
 import { PopupContext } from "../../provider/PopupContext";
-import useFetch from "../../hooks/useFetch";
 
-const AddPopup = () => {
-  const { currentTable } = useContext(TableContext);
+const EditPopup = () => {
+  const { currentTable, selectedItems } = useContext(TableContext);
   const { showPopup } = useContext(PopupContext);
-  const { data } = useFetch();
 
-  useEffect(() => {
-    console.log(currentTable);
-  }, [currentTable]);
   return (
     <div className="input-area">
       {currentTable?.keys.map(
         (key, index) =>
           index > 0 && (
-            <input type="text" key={key} name={key} placeholder={currentTable.labels[index]} autoFocus={index === 1} />
+            <input
+              type="text"
+              key={key}
+              name={key}
+              defaultValue={selectedItems[0][key]}
+              placeholder={currentTable.labels[index]}
+              autoFocus={index === 1}
+            />
           )
       )}
       <button>{showPopup}</button>
@@ -25,4 +27,4 @@ const AddPopup = () => {
   );
 };
 
-export default AddPopup;
+export default EditPopup;
