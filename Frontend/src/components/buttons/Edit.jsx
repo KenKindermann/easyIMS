05a37d239e47619882dc85e2081e009b6cecc4a8
kenkindermann <ken.kindermann@gmail.com>
2024-02-05 21:48:36 +0100

    check if more or less than one item is selected and show an alert if so

--- Frontend/src/components/buttons/Edit.jsx
+++ Frontend/src/components/buttons/Edit.jsx
@@ -1,15 +1,32 @@
 import "../../style/button.css";
 import editIcon from "../../assets/icons/edit_FILL0_wght400_GRAD0_opsz24.svg";
 import usePopup from "../../hooks/usePopup";
+import { useContext } from "react";
+import { TableContext } from "../../provider/TableContext";
 
-const Search = () => {
+const Edit = () => {
+  const { selectedItems } = useContext(TableContext);
   const { openPopup } = usePopup();
 
+  const handleClick = () => {
+    const numberOfSelectedItems = selectedItems.length;
+    if (numberOfSelectedItems !== 1) {
+      showAlert(numberOfSelectedItems);
+    } else {
+      openPopup("Edit");
+    }
+  };
+
+  const showAlert = (quantity) => {
+    const message = quantity > 1 ? "More than on entry selected." : "No entry selected.";
+    alert(message);
+  };
+
   return (
-    <button onClick={() => openPopup("Edit")}>
+    <button onClick={handleClick}>
       <img src={editIcon} alt={"edit icon"} /> Edit
     </button>
   );
 };
 
-export default Search;
+export default Edit;
