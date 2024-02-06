1b6437e37ac00a7f2a2c0fc158842a221a193191
kenkindermann <ken.kindermann@gmail.com>
2024-02-06 10:15:03 +0100

    add handleSubmit function, form data is send as json postData function

--- Frontend/src/components/popups/AddPopup.jsx
+++ Frontend/src/components/popups/AddPopup.jsx
@@ -2,21 +2,32 @@ import { useContext } from "react";
 import "../../style/popup.css";
 import { TableContext } from "../../provider/TableContext";
 import { PopupContext } from "../../provider/PopupContext";
+import useAxios from "../../hooks/useAxios";
 
 const AddPopup = () => {
   const { currentTable } = useContext(TableContext);
   const { showPopup } = useContext(PopupContext);
+  const { postData } = useAxios();
+
+  const url = `http://localhost:8000/${currentTable.title}`;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    postData(url, data);
+  };
 
   return (
-    <div className="input-area">
+    <form className="input-area" onSubmit={(e) => handleSubmit(e)}>
       {currentTable?.keys.map(
         (key, index) =>
           index > 0 && (
             <input type="text" key={key} name={key} placeholder={currentTable.labels[index]} autoFocus={index === 1} />
           )
       )}
-      <button>{showPopup}</button>
-    </div>
+      <button type="submit">{showPopup}</button>
+    </form>
   );
 };
 
