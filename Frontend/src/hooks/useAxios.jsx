d9119ebdaafbeb4bc6850da0e9585ee06a4e39bc
kenkindermann <ken.kindermann@gmail.com>
2024-02-06 10:06:57 +0100

    renamed useFetch to useAxios, add postData function

--- Frontend/src/hooks/useAxios.jsx
+++ Frontend/src/hooks/useAxios.jsx
@@ -2,11 +2,11 @@ import { useContext, useState } from "react";
 import axios from "axios";
 import { TableContext } from "../provider/TableContext";
 
-const useFetch = () => {
+const useAxios = () => {
   const [error, setError] = useState(null);
   const { data, setData } = useContext(TableContext);
 
-  const fetchAll = async (url) => {
+  const getData = async (url) => {
     try {
       const response = await axios.get(url);
       setData(response.data);
@@ -15,7 +15,16 @@ const useFetch = () => {
     }
   };
 
-  return { error, fetchAll };
+  const postData = async (url, data) => {
+    try {
+      await axios.post(url, data);
+      getData(url);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  return { error, getData, postData };
 };
 
-export default useFetch;
+export default useAxios;
