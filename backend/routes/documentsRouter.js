3f46c2e53d28bce79c0b0c4ff3493d23c89c9b7f
kenkindermann <ken.kindermann@gmail.com>
2024-02-11 18:45:05 +0100

    implement addInvoice function and invoices post route

--- backend/routes/documentsRouter.js
+++ backend/routes/documentsRouter.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { deleteById, getAll, getByValue, sortByValue } from "../controllers/general.js";
+import { addInvoice } from "../controllers/invoices.js";
 
 const documentsRouter = Router();
 
-documentsRouter.route("/invoices").get((req, res) => getAll("invoices", req, res));
+documentsRouter
+  .route("/invoices")
+  .get((req, res) => getAll("invoices", req, res))
+  .post(addInvoice);
+
 documentsRouter.route("/invoices/search").get((req, res) => getByValue("invoices", req, res));
 
 export default documentsRouter;
