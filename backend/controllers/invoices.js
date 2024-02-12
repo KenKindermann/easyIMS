7dc688060250e9fe495c2ab476db1c7022afd4f5
kenkindermann <ken.kindermann@gmail.com>
2024-02-12 09:37:10 +0100

    implement addInvoiceProducts function and add route for this

--- backend/controllers/invoices.js
+++ backend/controllers/invoices.js
@@ -1,13 +1,28 @@
 import pool from "../db/server.js";
 
 export const addInvoice = async (req, res) => {
-  console.log("Hello");
   const { date, firstname, lastname, street, zipcode, city, shippingstreet, shippingzipcode, shippingcity } = req.body;
 
   try {
     const response = await pool.query(
       "INSERT INTO invoices (date, firstname, lastname, street, zipcode, city, shippingstreet, shippingzipcode, shippingcity) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
       [date, firstname, lastname, street, zipcode, city, shippingstreet, shippingzipcode, shippingcity]
+    );
+    res.json(response.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error });
+  }
+};
+
+export const addInvoiceProducts = async (req, res) => {
+  console.log("Hello");
+  const { invoice_id, id, ean, producer, details, tax, gross_retail_price } = req.body;
+
+  try {
+    const response = await pool.query(
+      "INSERT INTO invoice_products (invoice_id, id, ean, producer, details, tax, gross_retail_price) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [invoice_id, id, ean, producer, details, tax, gross_retail_price]
     );
     res.json(response.rows);
   } catch (error) {
--- backend/routes/documentsRouter.js
+++ backend/routes/documentsRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { deleteById, getAll, getByValue, sortByValue } from "../controllers/general.js";
-import { addInvoice } from "../controllers/invoices.js";
+import { addInvoice, addInvoiceProducts } from "../controllers/invoices.js";
 
 const documentsRouter = Router();
 
@@ -8,6 +8,8 @@ documentsRouter
   .route("/invoices")
   .get((req, res) => getAll("invoices", req, res))
   .post(addInvoice);
+
+documentsRouter.route("/invoices/invoice_products").post(addInvoiceProducts);
 
 documentsRouter.route("/invoices/search").get((req, res) => getByValue("invoices", req, res));
 
