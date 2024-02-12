99aca24d97c196ea83d50cc2366a7cae04e75c38
kenkindermann <ken.kindermann@gmail.com>
2024-02-12 12:20:02 +0100

    add customer_id to addInvoice function

--- backend/controllers/invoices.js
+++ backend/controllers/invoices.js
@@ -1,12 +1,23 @@
 import pool from "../db/server.js";
 
 export const addInvoice = async (req, res) => {
-  const { date, firstname, lastname, street, zipcode, city, shippingstreet, shippingzipcode, shippingcity } = req.body;
+  const {
+    date,
+    customer_id,
+    firstname,
+    lastname,
+    street,
+    zipcode,
+    city,
+    shippingstreet,
+    shippingzipcode,
+    shippingcity,
+  } = req.body;
 
   try {
     const response = await pool.query(
-      "INSERT INTO invoices (date, firstname, lastname, street, zipcode, city, shippingstreet, shippingzipcode, shippingcity) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
-      [date, firstname, lastname, street, zipcode, city, shippingstreet, shippingzipcode, shippingcity]
+      "INSERT INTO invoices (date, customer_id, firstname, lastname, street, zipcode, city, shippingstreet, shippingzipcode, shippingcity) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
+      [date, customer_id, firstname, lastname, street, zipcode, city, shippingstreet, shippingzipcode, shippingcity]
     );
     res.json(response.rows);
   } catch (error) {
