d673c1c4224bce61a4c51102185556af963ce855
kenkindermann <ken.kindermann@gmail.com>
2024-02-13 11:18:51 +0100

    implement fetchData useEffect for invoice and credit documents

--- Frontend/src/components/pages/Documents.jsx
+++ Frontend/src/components/pages/Documents.jsx
@@ -1,7 +1,7 @@
 import Table from "../table/Table.jsx";
 import ControlPanel from "../controlPanel/ControlPanel";
 import { OpenPopup, Sort } from "../formControls/Buttons";
-import { products } from "../../utils/tableFormatter.js";
+import { documents, products } from "../../utils/tableFormatter.js";
 import useAxios from "../../hooks/useAxios.jsx";
 import { useContext, useEffect, useState } from "react";
 import { DataContext } from "../../provider/DataContext.jsx";
@@ -15,8 +15,30 @@ const Documents = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    setActiveState({ title: "Documents", setData: setDocumentData, table: products });
+    setActiveState({ title: "Documents", setData: setDocumentData, table: documents });
   }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const invoicesUrl = `http://localhost:8000/documents/invoices`;
+      const invoices = await getData(invoicesUrl);
+
+      const creditsUrl = `http://localhost:8000/documents/credits`;
+      const credits = await getData(creditsUrl);
+
+      if (invoices && credits) {
+        const documents = [...invoices, ...credits];
+        const dateAndTime = documents.map((doc) => ({
+          ...doc,
+          dateAndTime: `${doc.date} ${doc.time}`,
+        }));
+
+        const sortedDocuments = dateAndTime.sort((a, b) => new Date(b.dateAndTime) - new Date(a.dateAndTime));
+        setDocumentData([...sortedDocuments]);
+      }
+    };
+    fetchData();
+  }, [activeState]);
 
   const DropDownItems = [
     {
@@ -37,7 +59,7 @@ const Documents = () => {
           <OpenPopup key="search" title="Search" />,
         ]}
       />
-      <Table />
+      <Table data={documentData} />
     </>
   );
 };
